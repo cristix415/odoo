8dd57a99b7fd33f5372520de76fbebbfd9c34b8a
Blagoescu Cristian <cristix415@gmail.com>
2025-12-24 15:49:54 +0200

    test

--- ModulCristi/models/modul_cristi.py
+++ ModulCristi/models/modul_cristi.py
@@ -5,5 +5,5 @@ class ModulCristi(models.Model):
     _name = 'modul.cristi'
     _description = 'Înregistrare Modul Cristi'
 
-    name = fields.Char(string='Nume', required=True)
+    name = fields.Char(string='Numee', required=True)
     description = fields.Text(string='Descriere')
